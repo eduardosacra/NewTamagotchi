dc2d22b468e6090e9302748ebdeef9e8c88c543c
Eduardo <eduardo.sacra.sousa@gmail.com>
2022-12-24 03:19:15 -0300

    Iniciado a tela de visualizacao

--- NewTamagotchi/Program.cs
+++ NewTamagotchi/Program.cs
@@ -2,76 +2,88 @@
 
 using NewTamagotchiLib.Model;
 using NewTamagotchiLib.Service;
+using NewTamagotchiLib.View;
 using System.Linq;
 
-string opcaoMenu = "";
-PokemonPokeAPI monstroSelecionado = new PokemonPokeAPI();
-bool pokemonFoiSelecionado = false;
-int pokemonEscolhidoMenu;
-IPokemonService pokeService = new PokemonServiceAPI();
+MenuIterativo menu = new MenuIterativo();
 
 
-Console.WriteLine("Start - precione Enter");
-opcaoMenu = Console.ReadLine();
+menu.BoasVindas();
 
-var monstros = await pokeService.ConsultarCatalogo();
+menu.ExibirChocando();
 
-Console.WriteLine("Escolha seu monstrinho");
+menu.ExibirTitulo(" Adotar um Mascote ");
+menu.ExibirTitulo();
+menu.ExibirTitulo(" menu ");
+
+//string opcaoMenu = "";
+//PokemonPokeAPI monstroSelecionado = new PokemonPokeAPI();
+//bool pokemonFoiSelecionado = false;
+//int pokemonEscolhidoMenu;
+//IPokemonService pokeService = new PokemonServiceAPI();
 
 
-Console.WriteLine();
-Console.WriteLine();
+//Console.WriteLine("Start - precione Enter");
+//opcaoMenu = Console.ReadLine();
+
+//var monstros = await pokeService.ConsultarCatalogo();
+
+//Console.WriteLine("Escolha seu monstrinho");
 
 
-while (!pokemonFoiSelecionado) 
-{
-    pokemonEscolhidoMenu = 0;
-    int contador = 0;
-    foreach (var monstro in monstros.results.ToList())
-    {
-        Console.WriteLine("{0} - {1}", contador++, monstro.name);
-    }
+//Console.WriteLine();
+//Console.WriteLine();
 
-    opcaoMenu = Console.ReadLine();
-    if (opcaoMenu.Length > 0 && int.TryParse(opcaoMenu, out pokemonEscolhidoMenu))
-        monstroSelecionado = pokemonEscolhidoMenu < contador ? monstros.results[pokemonEscolhidoMenu] : null;
 
-    if (monstroSelecionado == null)
-    {
-        Console.WriteLine();
-        Console.WriteLine("Escolha uma opcao valida");
-        Console.WriteLine();
+//while (!pokemonFoiSelecionado) 
+//{
+//    pokemonEscolhidoMenu = 0;
+//    int contador = 0;
+//    foreach (var monstro in monstros.results.ToList())
+//    {
+//        Console.WriteLine("{0} - {1}", contador++, monstro.name);
+//    }
 
-    }
-    else
-        pokemonFoiSelecionado = true;
+//    opcaoMenu = Console.ReadLine();
+//    if (opcaoMenu.Length > 0 && int.TryParse(opcaoMenu, out pokemonEscolhidoMenu))
+//        monstroSelecionado = pokemonEscolhidoMenu < contador ? monstros.results[pokemonEscolhidoMenu] : null;
 
-    Console.WriteLine();
+//    if (monstroSelecionado == null)
+//    {
+//        Console.WriteLine();
+//        Console.WriteLine("Escolha uma opcao valida");
+//        Console.WriteLine();
 
-} 
+//    }
+//    else
+//        pokemonFoiSelecionado = true;
 
-Console.WriteLine($"Monstro Selecionado foi o {monstroSelecionado.name}");
+//    Console.WriteLine();
 
-Console.WriteLine();
+//} 
 
-Console.WriteLine("Consultar Detalhes sobre o seu Monstrinho");
-var pokemon = await pokeService.ConsultarPokemon(monstroSelecionado);
-Console.WriteLine("-------------------------------------------");
-Console.WriteLine($"Nome Pokemon: {pokemon.name} - Altura: {pokemon.height} - Peso: {pokemon.weight}");
-Console.WriteLine("Tipo:");
-foreach (var item in pokemon.types)
-{
-    Console.WriteLine($"** {item.type.name}");
-}
+//Console.WriteLine($"Monstro Selecionado foi o {monstroSelecionado.name}");
 
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine($"Habilidades:");
-foreach (var habilidade in pokemon.abilities)
-{
-    Console.WriteLine($"** {habilidade.ability.name}");
-}
+//Console.WriteLine();
 
-Console.WriteLine();
-Console.WriteLine();
-Console.WriteLine("Excelente Escolha!!!");
+//Console.WriteLine("Consultar Detalhes sobre o seu Monstrinho");
+//var pokemon = await pokeService.ConsultarPokemon(monstroSelecionado);
+//Console.WriteLine("-------------------------------------------");
+//Console.WriteLine($"Nome Pokemon: {pokemon.name} - Altura: {pokemon.height} - Peso: {pokemon.weight}");
+//Console.WriteLine("Tipo:");
+//foreach (var item in pokemon.types)
+//{
+//    Console.WriteLine($"** {item.type.name}");
+//}
+
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine($"Habilidades:");
+//foreach (var habilidade in pokemon.abilities)
+//{
+//    Console.WriteLine($"** {habilidade.ability.name}");
+//}
+
+//Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine("Excelente Escolha!!!");
--- NewTamagotchiLib/View/MenuIterativo.cs
+++ NewTamagotchiLib/View/MenuIterativo.cs
@@ -11,14 +11,40 @@ namespace NewTamagotchiLib.View
         public void BoasVindas()
         {
             Console.WriteLine("      ###########");
-            Console.WriteLine("           #           ##    #      #   ##     ####     ####  "); 
-            Console.WriteLine("           #          #  #   ##    ##  #  #   #    #   #    # ");
-            Console.WriteLine("           #         #    #  #  ##  # #    #  #        #    # ");
-            Console.WriteLine("           #         ######  #      # ######  #   ###  #    # ");
-            Console.WriteLine("           #         #    #  #      # #    #  #     #  #    # "); 
-            Console.WriteLine("           #         #    #  #      # #    #   #####    ####  ");
+            Console.WriteLine("           #         ##    #      #   ##     ####     ####   #######   ####   #    #  #"); 
+            Console.WriteLine("           #        #  #   ##    ##  #  #   #    #   #    #     #     #    #  #    #  #");
+            Console.WriteLine("           #       #    #  #  ##  # #    #  #        #    #     #     #       ######  #");
+            Console.WriteLine("           #       ######  #      # ######  #   ###  #    #     #     #       #    #  #");
+            Console.WriteLine("           #       #    #  #      # #    #  #     #  #    #     #     #    #  #    #  #"); 
+            Console.WriteLine("           #       #    #  #      # #    #   #####    ####      #      ####   #    #  #");
 
 
+        }
+
+        public void ExibirChocando()
+        {
+            Console.WriteLine("       ###");
+            Console.WriteLine("      #####");
+            Console.WriteLine("     ########");
+            Console.WriteLine("     ########");
+            Console.WriteLine("     ########");
+            Console.WriteLine("      #######");
+            Console.WriteLine("       #####");
+        }
+
+        public void SeparadorMenu()
+        {
+            Console.WriteLine("----------------------------------------------------------");
+        }
+
+        public void ExibirTitulo(string titulo = "")
+        {            
+            while(titulo.Length <= 100)
+            {
+                titulo = "-" + titulo + "-";
+            }
+            titulo = titulo.ToUpper();
+            Console.WriteLine(titulo);
         }
     }
 }
